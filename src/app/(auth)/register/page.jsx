4153cbb398e06302eeb2a66c263727b0e8dd465d
Mohd Khalid Khan <159447394+khalidkhnz@users.noreply.github.com>
2024-02-24 17:23:15 +0530

    register page

--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import module from "./register.module.scss";
 import Image from "next/image";
-import COVER from "@/assets/parallax/p3.jpg";
+import COVER from "@/assets/parallax/p12.jpg";
+import { SignIn } from "@/components/login/Login";
 
 const page = () => {
   return (
@@ -22,7 +23,36 @@ function LeftPart() {
   );
 }
 function RightPart() {
-  return <div className={module.right__part}>RightPart</div>;
+  return (
+    <div className={module.right__part}>
+      <svg
+        className={module.svg__top}
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 1440 320"
+      >
+        <path
+          fill="#dd9946"
+          fill-opacity="1"
+          d="M0,160L60,176C120,192,240,224,360,213.3C480,203,600,149,720,128C840,107,960,117,1080,133.3C1200,149,1320,171,1380,181.3L1440,192L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"
+        ></path>
+      </svg>
+
+      <SignIn />
+      {/* <SignUp /> */}
+
+      <svg
+        className={module.svg__bottom}
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 1440 320"
+      >
+        <path
+          fill="#dd9946"
+          fill-opacity="1"
+          d="M0,160L60,176C120,192,240,224,360,213.3C480,203,600,149,720,128C840,107,960,117,1080,133.3C1200,149,1320,171,1380,181.3L1440,192L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"
+        ></path>
+      </svg>
+    </div>
+  );
 }
 
 export default page;
--- src/components/customButton/CustomButton.jsx
+++ src/components/customButton/CustomButton.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import Link from "next/link";
 import module from "./custombutton.module.scss";
 
-const CustomButton = ({ href, children }) => {
+const CustomButton = ({ href, width, children }) => {
   return (
-    <div className={module.custom__button__parent}>
+    <div
+      style={{ width: width ? width : "" }}
+      className={module.custom__button__parent}
+    >
       <Link href={`${href}`}>
         <h4>
           <span className={module.gallery__button}>{children}</span>
